5983e56ab40739267c9959dd56cf9517a54adfcf
Trivaxy <itsmeomar2003@yahoo.com>
2023-01-08 18:59:34 +0300

    make SageGui more streamlined

--- src/main/java/team/lodestar/sage/ClientEvents.java
+++ src/main/java/team/lodestar/sage/ClientEvents.java
@@ -1,16 +1,29 @@
 package team.lodestar.sage;
 
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderGuiEvent;
 import net.minecraftforge.client.event.RenderLevelStageEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import team.lodestar.sage.client.graphics.notification.NotificationRenderer;
+import team.lodestar.sage.client.gui.SageGui;
+import team.lodestar.sage.client.gui.components.TextComponent;
+
+import java.awt.*;
 
 @Mod.EventBusSubscriber(modid = SageMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class ClientEvents {
+    private static SageGui TEST_GUI = new SageGui(() -> new TextComponent("Hello World!", Color.WHITE));
+
     @SubscribeEvent
     public static void onRenderLevel(RenderLevelStageEvent event) {
         if (event.getStage() == RenderLevelStageEvent.Stage.AFTER_WEATHER)
             NotificationRenderer.renderNotifications(event.getPoseStack(), event.getCamera(), event.getPartialTick());
+    }
+
+    @SubscribeEvent
+    public static void onRenderGui(RenderGuiEvent.Post event) {
+        SageGui.registerGui(TEST_GUI);
+        SageGui.renderAll(event.getPoseStack(), -1, -1, event.getPartialTick());
     }
 }
--- src/main/java/team/lodestar/sage/client/gui/SageGui.java
+++ src/main/java/team/lodestar/sage/client/gui/SageGui.java
@@ -3,10 +3,17 @@ package team.lodestar.sage.client.gui;
 import com.mojang.blaze3d.vertex.PoseStack;
 import team.lodestar.sage.client.gui.components.UIComponent;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Supplier;
 
 public class SageGui {
+
+    // i could use a registry, but for now this will do fine
+    private static List<SageGui> guis = new ArrayList<>();
+
     private SageDisplay display;
+    private boolean visible = true;
 
     public SageGui(Supplier<UIComponent> componentSupplier) {
         display = new SageDisplay(componentSupplier);
@@ -14,5 +21,21 @@ public class SageGui {
 
     public void render(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
         display.render(poseStack, mouseX, mouseY, partialTicks);
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public static void renderAll(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
+        for (SageGui gui : guis) {
+            if (gui.visible)
+                gui.render(poseStack, mouseX, mouseY, partialTicks);
+        }
+    }
+
+    public static void registerGui(SageGui gui) {
+        if (!guis.contains(gui))
+            guis.add(gui);
     }
 }
